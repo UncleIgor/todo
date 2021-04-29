31f5da7049d05f5bcf7be8b279d7931957f7e589
Igor <ig.pershin2011@ya.ru>
2021-04-29 15:44:48 +0300

    Первый вариант архитектуры

--- todo.py
+++ todo.py
@@ -1,143 +1,128 @@
 #!/usr/bin/env python3
 
 import re
+import datetime
 
-DEF_TASK_PARAMS=(("dates", 0), ("content", 0), ("tags", 0), ("indent", 0), ("chekbox", 0))
-
-def reset_dict():
-    dsc_task.clear()
-    dsc_task['chekbox']=0 #bool
-    dsc_task['content']=None
-    dsc_task['dates']=None
-    dsc_task['tags']=None
-    dsc_task['indent']=0 #int
-    dsc_task['prio']=0 #int
-
-def parsing_task(in_str):
+def parsing_task(task_str):
     dsc_task=dict()
 
-    if not in_str:
+    if not task_str:
         return 1;
 
-    if in_str.isspace():
+    if task_str.isspace():
         return dsc_task
     
     #Вычисляем отступ
-    indent=re.match('^\s+', in_str)
+    indent=re.match('^\s+', task_str)
     if indent:
-        dsc_task['indent']=(indent.end()//4)*4
+        dsc_task['indent']=round(indent.end()/4)*4
     
     #Статус задачи
-    if re.search('\[x\]', in_str):
+    if re.search('\[x\]', task_str):
         dsc_task['chekbox']=1        
 
-    #Дата 
-    date=re.findall('\[(\d{1,2}\.\d{1,2}\.\d{2,4}.*)\]', in_str)
-    if date:
-        dsc_task['dates']=''.join(date)
+    #Дата_начала 
+    start_date=re.findall('\[(\d{1,2}\.\d{1,2}\.\d{2,4}).*\]', task_str)
+    if start_date:
+        dsc_task['start_date']=''.join(start_date)
+        task_str=task_str.replace(dsc_task['start_date'], "")
     
+    #Дата_завершения
+    close_date=re.findall('\[.-.(\d{1,2}\.\d{1,2}\.\d{2,4})\]', task_str)
+    if close_date:
+        dsc_task['close_date']=''.join(close_date)
+        task_str=re.sub('\[.-.(\d{1,2}\.\d{1,2}\.\d{2,4})\]', '', task_str)
+        
     #Теги
-    dsc_task['tags']="NO_ЕФПЫ"
-    tags = re.findall(r'[@,+,#]\S+', in_str)
+    tags = re.findall(r'[@,+,#]\S+', task_str)
+    if tags:
+        dsc_task['tags']=' '.join(tags)
+        for tag in tags:
+            task_str=task_str.replace(tag, "")
+
+    #Содержание
+    task_str=re.sub('\[\]', '', task_str)
+    task_str=re.match('^\s*(.+)\s+', task_str)
+    if task_str:
+        dsc_task['content']=re.sub(r'\s\s+', '', task_str.group(1))
     
     return dsc_task
-    
-
-    dsc_task['content'] = re.sub(r"\[x\]|\s+", ' ', in_str)
-    
-    if dsc_task['dates']:
-       print(1)
-       dsc_task['content'] = re.findall(r'^\s*(.+)\s+\[', dsc_task['content'])
-    else:
-       print(2)
-       dsc_task['content'] = re.findall(r'^\s*(.+)\s*', dsc_task['content'])
-
-    #print (dsc_task['content'])
-    tags = re.findall(r'[@,+,#]\S+', in_str)
-    if tags:
-        dsc_task['tags']=tags
-        myString = ''.join(dsc_task['content'])
-        for list_pattern in dsc_task['tags']:
-            myString = myString.replace(list_pattern, '')
-            #print (myString)
-        dsc_task['content'] = myString
-    #print ("Стока:", num_in_str, "Состояние", dsc_task['chekbox'], dsc_task['dates'], dsc_task['content'], "Отступ", dsc_task['indent'], dsc_task['tags'])
-    return 0
-
-def add_def_params_2_task(task):
-    if not task:
-        return dict()
-
-    for parameters in DEF_TASK_PARAMS:
-        parameter, value = parameters
-        if not parameter in task:
-            task[parameter]=value
-    
-    return task
 
 def processing_task(task):
-    if not task or task[content] == 0:
-        return "EMPTY_STR"
-    return "blablabla"
+    indent=task.get("indent", 0)
+    tags=task.get("tags", None)
+    content=task.get("content", None)
 
-
-def print_task(task):
+    #Пустые строки
     if not task:
-        print ("Пустая строка")
-        return 0
+        return task
 
-    print(task, len(task) )
-    return 0
-    for item in task:
-        if not task['dates']:
-            task['dates']=""
-
-    print("Отступ:", task['indent'], "Статус:", task['chekbox'], "Дата:", task['dates'])
-    return 0
-
-def print_format_task():
-    chekbox=""
-    indent=""
-    tags=""
+    #Добавление даты начала
+    if not task.get("start_date", None):
+        now = datetime.datetime.now()
+        task["start_date"]=now.strftime("%d.%m.%Y")
     
-    tmp_f = open('tmp', 'w')
-    for task in DOC_LIST:
-        print(task['content'])
-        if task['tags'] != None:
-            tags=''.join(task['tags'])
-        
-        if task['chekbox'] == 1:
-            chekbox="[x]"
-            
-        if task['dates'] == None:
-            dates="[11.22.3333]"
+    #Добавление даты завершения
+    if task.get("chekbox", None):
+        now = datetime.datetime.now()
+        task["close_date"]=now.strftime("%d.%m.%Y")
+
+    #Обработка тегов
+
+    return task
+
+def print_todo_2_file(todo_list):
+
+    if not todo_list:
+        print ("Пустое TODO")
+        return 1
+
+    try:
+        tmp_f = open('tmp', 'w')
+    except:
+        print ("Не смог создать временный файл")
+        return 2
+
+    for task in todo_list:
+        if not task:
+            tmp_f.write('\n')
+            continue
+
+        content=task.get("content", None)
+        indent=task.get("indent", 0)
+        chekbox=task.get("chekbox", "")
+        start_date=task.get("start_date", "")
+        close_date=task.get("close_date", None)
+        tags=task.get("tags", "")
+                
+        if close_date:
+            date='[{0} - {1}]'.format(start_date, close_date)
         else:
-            dates=''.join("%s" % task['dates'])
+            date='[{0}]'.format(start_date)
+
+        indent=''.rjust(indent, " ")
+
+        tmp_f.write(''.join("%s%-80s %-27s %s\n" % (indent, content, date, tags)))
         
-        if task['content'] != None:
-            myString=''.join("%s%3s %-80s %-27s %s\n" % (indent, chekbox, task['content'], dates, tags))
-        else:
-            myString='\n'
-        
-        tmp_f.write(myString)
-    
     tmp_f.close()
 
 def main(file_name):
     todo_list = list();
 
     try:
-        file_handler = open(file_name, 'r')
+        file_handler = open(file_name, 'r', encoding="utf-8")
     except IOError:
         print("Файла не существует")
-        exit(0)
-        
+        exit(1)
+    
     for line in file_handler:
         task=parsing_task(line)
-        task=add_def_params_2_task(task)
         task=processing_task(task)
-        print_task(task)
+        todo_list.append(task)
     file_handler.close()
+
+    #Сортировка
+    print_todo_2_file(todo_list)
 
 main("TODO")
 
