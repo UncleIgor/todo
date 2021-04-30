6728dea3f94d63a6beea6d06a2c451a894ffda18
Igor <ig.pershin2011@ya.ru>
2021-04-30 16:15:52 +0300

    Добавляю дерево

--- todo.py
+++ todo.py
@@ -15,7 +15,7 @@ def parsing_task(task_str):
     #Вычисляем отступ
     indent=re.match('^\s+', task_str)
     if indent:
-        dsc_task['indent']=round(indent.end()/4)*4
+        dsc_task['indent']=round(indent.end()/4)
     
     #Статус задачи
     if re.search('\[x\]', task_str):
@@ -34,11 +34,12 @@ def parsing_task(task_str):
         task_str=re.sub('\[.-.(\d{1,2}\.\d{1,2}\.\d{2,4})\]', '', task_str)
         
     #Теги
-    tags = re.findall(r'[@,+,#]\S+', task_str)
-    if tags:
-        dsc_task['tags']=' '.join(tags)
-        for tag in tags:
+    dsc_task['tags']=re.findall(r'[@,+,#]\S+', task_str)
+    if dsc_task['tags']:
+        for tag in dsc_task['tags']:
             task_str=task_str.replace(tag, "")
+            if "#pr:" in tag:
+                dsc_task['prio']=int(tag.replace("#pr:", ""))
 
     #Содержание
     task_str=re.sub('\[\]', '', task_str)
@@ -93,21 +94,39 @@ def print_todo_2_file(todo_list):
         chekbox=task.get("chekbox", "")
         start_date=task.get("start_date", "")
         close_date=task.get("close_date", None)
-        tags=task.get("tags", "")
-                
+        tags=' '.join(task.get("tags", ""))
+        
         if close_date:
             date='[{0} - {1}]'.format(start_date, close_date)
         else:
             date='[{0}]'.format(start_date)
 
-        indent=''.rjust(indent, " ")
+        indent=''.rjust(indent*4, " ")
 
-        tmp_f.write(''.join("%s%-80s %-27s %s\n" % (indent, content, date, tags)))
+        tmp_f.write(''.join("%s%-80s %s %s\n" % (indent, content, date, tags)))
         
     tmp_f.close()
 
+def sort_by_prio(todo_list):
+    if not todo_list:
+        print ("Пустое TODO")
+        return 1
+
+    sort_todo=[]
+    
+    for task in todo_list:
+        if not task:
+            continue
+        
+        indent=task.get("indent", 0)
+        prio=task.get("prio", -1)
+        print(prio)
+
 def main(file_name):
-    todo_list = list();
+    idx_deph=list()
+    idx_deph.append(-1)
+    depth=0
+    todo_list=list()
 
     try:
         file_handler = open(file_name, 'r', encoding="utf-8")
@@ -118,12 +137,101 @@ def main(file_name):
     for line in file_handler:
         task=parsing_task(line)
         task=processing_task(task)
+        #print(task.get("indent", 0))
+        cur_depth=task.get("indent", 0)
+        
+        if cur_depth == depth:
+            idx_deph[depth]+=1
+        elif cur_depth > depth:
+            depth=cur_depth
+            idx_deph.insert(depth, 0)
+        elif cur_depth < depth:
+            up_depth=depth-cur_depth
+            for number in range(up_depth):
+                idx_deph.pop(-1)
+            depth=cur_depth
+            idx_deph[depth]+=1
+        print("Путь строки", idx_deph)
         todo_list.append(task)
+        
     file_handler.close()
 
     #Сортировка
-    print_todo_2_file(todo_list)
+    #sort_by_prio(todo_list)
+    
+#    print_todo_2_file(todo_list)
 
-main("TODO")
+# main("TODO1")
 
 
+# depth - переключатель глубины
+# last_depth_idx - последний индекс на текущей глубине
+# cur_depth - текщая глубина
+# prev_depth - глубина предыдущей задачи
+# idx_deph_mas - массив индексов на каждой глубине [0, 0, 0]
+
+
+
+
+
+def save_task(puth, task):
+    db_list=TODO_DB
+    interator=0
+    print(puth)
+    size_puth=len(puth)
+    for node in puth:
+       /interator+=1
+        print(interator, size_puth)
+        if interator > size_puth:
+            db_list=list()
+            db_list.append(task)
+            return 0
+        # try:
+        
+        if node == 0:
+            print(node, len(db_list))
+            db_list=db_list[node]
+        else:
+            db_list=db_list[node-1]
+        # except:
+        #     db_list.insert(node, task)
+        #     return 0
+    #print("----------------------")
+
+TODO_DB=list()
+idx_deph=list()
+idx_deph.append(-1)
+depth=0
+# test_deph=(0, 0, 1, 2, 2, 1, 2, 2, 3, 3, 0, 1, 0)
+
+for cur_depth in test_deph:    
+    if cur_depth == depth:
+        idx_deph[depth]+=1
+    elif cur_depth > depth:
+        depth=cur_depth
+        idx_deph.insert(depth, 0)
+    elif cur_depth < depth:
+        up_depth=depth-cur_depth
+        for number in range(up_depth):
+            idx_deph.pop(-1)
+            depth=cur_depth
+            idx_deph[depth]+=1
+    #print("Путь строки", idx_deph)
+    save_task(idx_deph, list(range(5)))
+    #print(TODO_DB)
+        #TODO_DB.append(list(range(1)))
+print(TODO_DB)
+
+    # if cur_depth == depth:
+    #     idx_deph[depth]+=1
+    # elif cur_depth > depth:
+    #     depth=cur_depth
+    #     idx_deph.insert(depth, 0)
+    # elif cur_depth < depth:
+    #     up_depth=depth-cur_depth
+    #     for number in range(up_depth):
+    #         idx_deph.pop(-1)
+    #     depth=cur_depth
+    #     idx_deph[depth]+=1
+    # save_task(idx_deph)        
+#    print("Путь строки", idx_deph)
